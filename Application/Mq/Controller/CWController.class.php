fd4d6477211303b987a20f6376e2456d721b1f8b
lizhelin <lizhelin@ygsoft.com>
2017-08-22 19:15:37 +0800

    财务收款

---
+++ Application/Mq/Controller/CWController.class.php
@@ -0,0 +1,95 @@
+<?php
+namespace Mq\Controller;
+use Think\Controller;
+
+class CWController extends Controller {
+
+	public function _initialize() {
+		putHeader();
+	}
+	
+	
+	/**
+	 * 查询财务收款列表.
+	 */
+	public function findCwsk() {
+		$query = new \Think\Model();
+		$condition_sql = "";
+		$count_sql = "";
+		$query_sql = "";
+		$countquery_sql = "";
+		$cname = $_REQUEST['cname_search'];
+		if (!empty($cname)) {
+			$query_sql = $query_sql . " and cname like '%$cname%'";
+			$countquery_sql = $countquery_sql . " and cname like '%$cname%'";
+		}
+		
+		$iDisplayLength = intval($_REQUEST['iDisplayLength']);
+		$iDisplayStart = intval($_REQUEST['iDisplayStart']);
+		
+		$count_sql = "select count(*) as totalrecord from cwsk where 1=1 $countquery_sql";
+		$condition_sql = "select * from cwsk where 1=1 $query_sql limit $iDisplayStart,$iDisplayLength";
+		
+		$resultcount = $query -> query($count_sql);
+		$result = $query -> query($condition_sql);
+		$iTotalRecords = $resultcount[0]['totalrecord'];
+		$iDisplayLength = $iDisplayLength < 0 ? $iTotalRecords : $iDisplayLength;
+		$sEcho = intval($_REQUEST['sEcho']);
+		$records = array();
+		$records["aaData"] = array();
+		$jsparams = "keyword:$orderid,buyername:$updowntag";
+		
+		for ($i = 0; $i < count($result); $i++) {
+			$btnView = "<a class='btn btn-xs default'  data-toggle='modal' onclick=\"openView('".$result[$i]['pkid']."','".$iDisplayStart."','".$jsparams."')\"><i class='fa fa-pencil'></i> &nbsp;查看&nbsp;</a>";
+			$btnEdit = "&nbsp;&nbsp;<a class='btn btn-xs default'  data-toggle='modal' onclick=\"openEdit('".$result[$i]['pkid']."','".$iDisplayStart."','".$jsparams."')\"><i class='fa fa-pencil'></i> &nbsp;编辑&nbsp;</a>";
+			$btnDel = "&nbsp;&nbsp;<a class='btn btn-xs default'  data-toggle='modal' onclick=\"openDelConfirm('".$result[$i]['pkid']."','".$iDisplayStart."')\"><i class='fa fa-times'></i> &nbsp;删除&nbsp;</a>";
+			$opttime = date('Y-m-d',$result[$i]['opttime']);
+			$records["aaData"][] = array($result[$i]['cname'] , $opttime ,$result[$i]['totalmoney'],$btnView.$btnEdit.$btnDel);
+		}
+		if (isset($_REQUEST["sAction"]) && $_REQUEST["sAction"] == "group_action") {
+			$records["sStatus"] = "OK";
+			// pass custom message(useful for getting status of group actions)
+			$records["sMessage"] = "Group action successfully has been completed. Well done!";
+			// pass custom message(useful for getting status of group actions)
+		}
+		$records["sEcho"] = $sEcho;
+		$records["iTotalRecords"] = $iTotalRecords;
+		$records["iTotalDisplayRecords"] = $iTotalRecords;
+		echo json_encode($records);
+	}
+	/**
+	 * 删除车辆信息.
+	 */
+	public function delcwsk($pkid) {
+		$dao = M("Cwsk");				
+		$dao->where("pkid='$pkid'")->delete();
+		echo "yes";
+	}
+	
+	public function savecwsk(){
+		$obj = getObjFromPost(array("cname","opttime","totalmoney","bankmsg","remark"));
+		$obj['pkid'] = uniqid();
+		$obj['opttime'] = strtotime($obj['opttime']);
+		$dao = M("Cwsk");
+		$dao->add($obj);		
+		echo "yes";
+	}
+	
+	public function editcwsk(){
+		$obj = getObjFromPost(array("pkid","cname","opttime","totalmoney","bankmsg","remark"));
+		$pkid = $obj["pkid"];
+		$obj['opttime'] = strtotime($obj['opttime']);
+		$dao = M("Cwsk");
+		$dao->where("pkid='$pkid'")->save($obj);		
+		echo "yes";
+	}
+	
+	
+	public function loadcwsk($pkid){
+		$dao = M("Cwsk");
+		$result = $dao->where("pkid = '$pkid'")->find();		
+		header('Content-type: text/json');
+		header('Content-type: application/json');
+		echo json_encode($result, JSON_UNESCAPED_UNICODE);
+	}
+}
---
+++ admin/assets/scripts/admin/cw_sk_edit.js
@@ -0,0 +1,111 @@
+
+function bulidData() {
+	var util = new Util();
+	var opttime = $("#opttime").val();
+	if(util.isNullStr(send_obj.cname) || util.isNullStr(send_obj.totalmoney) || util.isNullStr(opttime)){
+		return false;
+	}
+	return true;
+}
+
+function saveData() {
+	var util = new Util();
+	$("#btnSave").button('loading');
+	var obj = bulidData();
+	if(obj == false) {
+		$("#btnSave").button("reset");
+		util.errorMsg('请填写完整信息');
+		return;
+	}
+	send_obj.opttime = $("#opttime").val();
+	send_obj.pkid = util.getParam("pkid");
+	if(obj != false) {
+		util.showLoading();
+		var url = "/Mq/CW/editcwsk";	
+		util.postUrl(
+			url,
+			function(data, status) { //如果调用php成功  
+				if(data == "yes") {
+					$("#btnSave").button("reset");
+					util.hideLoading();
+					util.successMsg('保存成功');
+					
+				} else {
+					$("#btnSave").button("reset");
+					util.hideLoading();
+					util.errorMsg('保存失败');
+				}
+			},
+			send_obj,
+			function(XMLHttpRequest, textStatus, errorThrown) {
+				util.errorMsg('内部服务器错误');
+				util.hideLoading();
+				$("#btnSave").button("reset");
+			}
+		);
+	}
+}
+var send_obj = {};
+var send_vue;
+
+function loadData() {
+	var util = new Util();	
+	var pkid = util.getParam("pkid");
+	var url = "/Mq/CW/loadcwsk/pkid/"+pkid;	
+		util.postUrl(
+			url,
+			function(data, status) { //如果调用php成功  
+				send_obj.cname=data.cname;
+				send_obj.totalmoney=data.totalmoney;
+				send_obj.bankmsg=data.bankmsg;
+				send_obj.remark=data.remark;
+				
+				send_vue = new Vue({
+					el:"#form_app",
+					data:{sendobj:send_obj}
+				});
+				$("#opttime").val(new Date(data.opttime*1000).Format("yyyy-MM-dd"));
+			},
+			function(XMLHttpRequest, textStatus, errorThrown) {
+				util.errorMsg('内部服务器错误');
+				util.hideLoading();
+				$("#btnSave").button("reset");
+			}
+		);
+	
+	
+}
+
+$(document).ready(function() {
+	if(jQuery().datepicker) {
+		$('.date-picker').datepicker({
+			rtl: App.isRTL(),
+			autoclose: true
+		});
+		$('body').removeClass("modal-open"); // fix bug when inline picker is used in modal
+	}
+	loadData();
+	
+	var util = new Util();
+	var type = util.getParam("type");
+	if(type=="display"){
+		$("#btnSave").hide();
+		$("#opttime").attr("disabled","disabled");
+		$("#cname").attr("disabled","disabled");
+		$("#totalmoney").attr("disabled","disabled");
+		$("#bankmsg").attr("disabled","disabled");
+		$("#opttime").attr("disabled","disabled");
+		$("#remark").attr("disabled","disabled");
+		$("#l_title").html("查看财务收款");
+		$("#m_title").html("查看财务收款信息");
+		$("#a_title").html("查看财务收款");
+	}else{
+		$("#l_title").html("编辑财务收款");
+		$("#m_title").html("编辑财务收款信息");
+		$("#a_title").html("编辑财务收款");
+		$("#btnSave").bind('click', function() {
+			saveData();
+		});
+	}
+	
+});
