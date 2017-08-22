ca7adde2a6e570a3a31f673bfe167cbb12603a72
lizhelin <lizhelin@ygsoft.com>
2017-08-22 22:49:59 +0800

    财务瓶换瓶收据

--- Application/Mq/Controller/CWController.class.php
+++ Application/Mq/Controller/CWController.class.php
@@ -92,4 +92,99 @@ class CWController extends Controller {
 		header('Content-type: application/json');
 		echo json_encode($result, JSON_UNESCAPED_UNICODE);
 	}
+	
+	
+	/**
+	 * 查询财务瓶换瓶收据列表.
+	 */
+	public function findCwphp() {
+		$query = new \Think\Model();
+		$condition_sql = "";
+		$count_sql = "";
+		$query_sql = "";
+		$countquery_sql = "";
+		$bname = $_REQUEST['bname_search'];
+		$lname = $_REQUEST['lname_search'];
+		$snumber = $_REQUEST['snumber_search'];
+		if (!empty($bname)) {
+			$query_sql = $query_sql . " and bname like '%$bname%'";
+			$countquery_sql = $countquery_sql . " and bname like '%$bname%'";
+		}
+		if (!empty($lname)) {
+			$query_sql = $query_sql . " and lname like '%$lname%'";
+			$countquery_sql = $countquery_sql . " and lname like '%$lname%'";
+		}
+		if (!empty($snumber)) {
+			$query_sql = $query_sql . " and snumber like '%$snumber%'";
+			$countquery_sql = $countquery_sql . " and snumber like '%$snumber%'";
+		}
+		
+		$iDisplayLength = intval($_REQUEST['iDisplayLength']);
+		$iDisplayStart = intval($_REQUEST['iDisplayStart']);
+		
+		$count_sql = "select count(*) as totalrecord from cwphp where 1=1 $countquery_sql";
+		$condition_sql = "select * from cwphp where 1=1 $query_sql limit $iDisplayStart,$iDisplayLength";
+		
+		$resultcount = $query -> query($count_sql);
+		$result = $query -> query($condition_sql);
+		$iTotalRecords = $resultcount[0]['totalrecord'];
+		$iDisplayLength = $iDisplayLength < 0 ? $iTotalRecords : $iDisplayLength;
+		$sEcho = intval($_REQUEST['sEcho']);
+		$records = array();
+		$records["aaData"] = array();
+		$jsparams = "keyword:$orderid,buyername:$updowntag";
+		
+		for ($i = 0; $i < count($result); $i++) {
+			$btnView = "<a class='btn btn-xs default'  data-toggle='modal' onclick=\"openView('".$result[$i]['pkid']."','".$iDisplayStart."','".$jsparams."')\"><i class='fa fa-pencil'></i> &nbsp;查看&nbsp;</a>";
+			$btnEdit = "&nbsp;&nbsp;<a class='btn btn-xs default'  data-toggle='modal' onclick=\"openEdit('".$result[$i]['pkid']."','".$iDisplayStart."','".$jsparams."')\"><i class='fa fa-pencil'></i> &nbsp;编辑&nbsp;</a>";
+			$btnDel = "&nbsp;&nbsp;<a class='btn btn-xs default'  data-toggle='modal' onclick=\"openDelConfirm('".$result[$i]['pkid']."','".$iDisplayStart."')\"><i class='fa fa-times'></i> &nbsp;删除&nbsp;</a>";
+			$opttime = date('Y-m-d',$result[$i]['opttime']);
+			$records["aaData"][] = array($result[$i]['bname'] , $opttime ,$result[$i]['totalmoney'],$result[$i]['snumber'],$result[$i]['lname'],$btnView.$btnEdit.$btnDel);
+		}
+		if (isset($_REQUEST["sAction"]) && $_REQUEST["sAction"] == "group_action") {
+			$records["sStatus"] = "OK";
+			// pass custom message(useful for getting status of group actions)
+			$records["sMessage"] = "Group action successfully has been completed. Well done!";
+			// pass custom message(useful for getting status of group actions)
+		}
+		$records["sEcho"] = $sEcho;
+		$records["iTotalRecords"] = $iTotalRecords;
+		$records["iTotalDisplayRecords"] = $iTotalRecords;
+		echo json_encode($records);
+	}
+	/**
+	 * 删除车辆信息.
+	 */
+	public function delcwphp($pkid) {
+		$dao = M("Cwphp");				
+		$dao->where("pkid='$pkid'")->delete();
+		echo "yes";
+	}
+	
+	public function savecwphp(){
+		$obj = getObjFromPost(array("bname","lname","snumber","opttime","totalmoney","msg","remark"));
+		$obj['pkid'] = uniqid();
+		$obj['opttime'] = strtotime($obj['opttime']);
+		$dao = M("Cwphp");
+		$dao->add($obj);		
+		echo "yes";
+	}
+	
+	public function editcwphp(){
+		$obj = getObjFromPost(array("pkid","bname","lname","snumber","opttime","totalmoney","msg","remark"));
+		$pkid = $obj["pkid"];
+		$obj['opttime'] = strtotime($obj['opttime']);
+		$dao = M("Cwphp");
+		$dao->where("pkid='$pkid'")->save($obj);		
+		echo "yes";
+	}
+	
+	
+	public function loadcwphp($pkid){
+		$dao = M("Cwphp");
+		$result = $dao->where("pkid = '$pkid'")->find();		
+		header('Content-type: text/json');
+		header('Content-type: application/json');
+		echo json_encode($result, JSON_UNESCAPED_UNICODE);
+	}
 }
---
+++ admin/assets/scripts/admin/cw_php_edit.js
@@ -0,0 +1,115 @@
+
+function bulidData() {
+	var util = new Util();
+	var opttime = $("#opttime").val();
+	if(util.isNullStr(send_obj.bname) || util.isNullStr(send_obj.totalmoney) || util.isNullStr(send_obj.snumber) || util.isNullStr(send_obj.lname) || util.isNullStr(opttime)){
+		return false;
+	}
+	return true;
+}
+
+function saveData() {
+	var util = new Util();
+	$("#btnSave").button('loading');
+	var obj = bulidData();
+	if(obj == false) {
+		$("#btnSave").button("reset");
+		util.errorMsg('请填写完整信息');
+		return;
+	}
+	send_obj.opttime = $("#opttime").val();
+	send_obj.pkid = util.getParam("pkid");
+	if(obj != false) {
+		util.showLoading();
+		var url = "/Mq/CW/editcwphp";	
+		util.postUrl(
+			url,
+			function(data, status) { //如果调用php成功  
+				if(data == "yes") {
+					$("#btnSave").button("reset");
+					util.hideLoading();
+					util.successMsg('保存成功');
+					
+				} else {
+					$("#btnSave").button("reset");
+					util.hideLoading();
+					util.errorMsg('保存失败');
+				}
+			},
+			send_obj,
+			function(XMLHttpRequest, textStatus, errorThrown) {
+				util.errorMsg('内部服务器错误');
+				util.hideLoading();
+				$("#btnSave").button("reset");
+			}
+		);
+	}
+}
+var send_obj = {};
+var send_vue;
+
+function loadData() {
+	var util = new Util();	
+	var pkid = util.getParam("pkid");
+	var url = "/Mq/CW/loadcwphp/pkid/"+pkid;	
+		util.postUrl(
+			url,
+			function(data, status) { //如果调用php成功  
+				send_obj.bname=data.bname;
+				send_obj.lname=data.lname;
+				send_obj.snumber=data.snumber;
+				send_obj.totalmoney=data.totalmoney;
+				send_obj.msg=data.msg;
+				send_obj.remark=data.remark;
+				
+				send_vue = new Vue({
+					el:"#form_app",
+					data:{sendobj:send_obj}
+				});
+				$("#opttime").val(new Date(data.opttime*1000).Format("yyyy-MM-dd"));
+			},
+			function(XMLHttpRequest, textStatus, errorThrown) {
+				util.errorMsg('内部服务器错误');
+				util.hideLoading();
+				$("#btnSave").button("reset");
+			}
+		);
+	
+	
+}
+
+$(document).ready(function() {
+	if(jQuery().datepicker) {
+		$('.date-picker').datepicker({
+			rtl: App.isRTL(),
+			autoclose: true
+		});
+		$('body').removeClass("modal-open"); // fix bug when inline picker is used in modal
+	}
+	loadData();
+	
+	var util = new Util();
+	var type = util.getParam("type");
+	if(type=="display"){
+		$("#btnSave").hide();
+		$("#opttime").attr("disabled","disabled");
+		$("#lname").attr("disabled","disabled");
+		$("#bname").attr("disabled","disabled");
+		$("#snumber").attr("disabled","disabled");
+		$("#totalmoney").attr("disabled","disabled");
+		$("#msg").attr("disabled","disabled");
+		$("#opttime").attr("disabled","disabled");
+		$("#remark").attr("disabled","disabled");
+		$("#l_title").html("查看财务瓶换瓶收据");
+		$("#m_title").html("查看财务瓶换瓶收据信息");
+		$("#a_title").html("查看财务瓶换瓶收据");
+	}else{
+		$("#l_title").html("编辑财务瓶换瓶收据");
+		$("#m_title").html("编辑财务瓶换瓶收据信息");
+		$("#a_title").html("编辑财务瓶换瓶收据");
+		$("#btnSave").bind('click', function() {
+			saveData();
+		});
+	}
+	
+});
